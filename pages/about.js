b192afb24007b7225d67571cee8ca549a4211d9c
Brady Corps <bcorps@RWB-MAC-LPT2.hitronhub.home>
2022-01-24 10:39:45 -0600

    Init Commit

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Layout from '@/components/Layout';
+
+const AboutPage = () => {
+	return (
+		<Layout title='About EnPointe and the RWB'>
+			<h1>About</h1>
+			<p>This is an app to find and view current RWB events</p>
+			<p>Version 2.0.0</p>
+			<p>Created by Brady Corps | Digital Content Coordinator</p>
+		</Layout>
+	);
+};
+
+export default AboutPage;
