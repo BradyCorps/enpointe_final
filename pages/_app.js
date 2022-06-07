1e61d492a033fab7225e79bd9e4921397e9bbb75
Brady Corps <bcorps@RWB-MAC-LPT2.local>
2022-06-07 10:54:39 -0500

    next/script Fix

--- pages/_app.js
+++ pages/_app.js
@@ -7,11 +7,12 @@ function MyApp({ Component, pageProps }) {
 	return (
 		<>
 			<Script
+				id='analytics-script'
 				strategy='lazyOnload'
 				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
 			/>
 
-			<Script strategy='lazyOnload'>
+			<Script id='analytics-script-public' strategy='lazyOnload'>
 				{`
 				window.dataLayer = window.dataLayer || [];
 				function gtag(){dataLayer.push(arguments);}
