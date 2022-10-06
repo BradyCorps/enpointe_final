fd6f9cb6775b34f2121efb518de6ef31fd08bd04
Brady Corps <bcorps@RWB-MAC-LPT2.hitronhub.home>
2022-10-06 14:59:25 -0500

    October Fix 3

--- pages/_app.js
+++ pages/_app.js
@@ -5,13 +5,11 @@ import '@/styles/index.scss';
 function MyApp({ Component, pageProps }) {
 	return (
 		<>
-			<SimpleReactLightbox>
-				<AuthProvider>
-					{/* Wrap component in AppContext */}
+			<AuthProvider>
+				{/* Wrap component in AppContext */}
 
-					<Component {...pageProps} />
-				</AuthProvider>
-			</SimpleReactLightbox>
+				<Component {...pageProps} />
+			</AuthProvider>
 		</>
 	);
 }
